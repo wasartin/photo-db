b10b64806bdbd76eb6578fd54a5e128ed2333b2a
wasartin <watisartin@gmail.com>
2022-01-03 21:54:38 -0600

    finish docker setup

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,8 +1,6 @@
-use pi_frame_displayable;
-
-CREATE TABLE [IF NOT EXISTS] photo (
+CREATE TABLE IF NOT EXISTS photo (
   pho_id serial PRIMARY KEY,
-  pho_absolute_file_path VARCHAR (260),
-  pho_name (260),
+  pho_absolute_file_path VARCHAR(260),
+  pho_name VARCHAR(260),
   pho_added_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP --    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
--- scripts/insertData.sql
+++ scripts/insertData.sql
@@ -1,7 +1,5 @@
-use pi_frame_displayable;
-
 INSERT INTO
-  photo (pho_absolute_file_path, pho_name)
+  photo(pho_absolute_file_path, pho_name)
 VALUES
   ('C:\Users\Will\Develop\workshop\piFrame\gallery\src\main\resources\static\posters\derzu_uzala.jpg', 'Derzu Uzala (1975)'),
   ('C:\Users\Will\Develop\workshop\piFrame\gallery\src\main\resources\static\posters\thing.jpg', 'The Thing (1982)');
